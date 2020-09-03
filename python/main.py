ec745e1e1b194f269c9c4095c617a4fe7e5113fb
Elcio Ferreira <elcio@visie.com.br>
2020-09-03 10:09:06 -0300

    Refactoring the runner, making it dynamic

--- python/main.py
+++ python/main.py
@@ -1,6 +1,20 @@
 import iterate_array
+import iterate_array2
 import recursive
+import recursive2
+import recursive3
+import whileLoop
 
+modules = {
+    'Iterate array': iterate_array,
+    'Iterate array (simpler)': iterate_array2,
+    'Recursive': recursive,
+    'Recursive (simpler)': recursive2,
+    'Recursive with memory cache': recursive3,
+    'While Loop': whileLoop,
+}
 
-print 'Iterate array - iterate_array.py', iterate_array.buildFibonacciSequence(15)
-print 'Recursive - recursive.py', recursive.buildFibonacciSequence(15)
+if __name__ == '__main__':
+    for description, module in modules.iteritems():
+        print description, '-', module.__name__+':', module.buildFibonacciSequence(15)
+
